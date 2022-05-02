12b357f8d51490c87427657d19990aafacf91941
Dmitry Odnorog <vibiy@yandex.ru>
2022-05-02 22:04:20 +0300

    412. Fizz buzz

---
+++ 412_fizz_buzz.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/fizz-buzz/
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n + 1):
+            if not i % 15:
+                result.append('FizzBuzz')
+            elif not i % 3:
+                result.append('Fizz')
+            elif not i % 5:
+                result.append('Buzz')
+            else:
+                result.append(str(i))
+
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    assert s.fizzBuzz(15) == [
+        "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz",
+    ]
