b79fd45c7e9297f59eeaff05e076bbf723e0cc1e
Dmitry Odnorog <vibiy@yandex.ru>
2022-05-07 23:20:51 +0300

    456. 132 Pattern

---
+++ 456_132_pattern.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/132-pattern/
+from typing import List
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+
+        second_number = float('-inf')
+        stack = []
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] < second_number:
+                return True
+            while stack and stack[-1] < nums[i]:
+                second_number = stack.pop()
+
+            stack.append(nums[i])
+
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert not s.find132pattern([1, 2, 3, 4])
+    assert s.find132pattern([3, 1, 4, 2])
+    assert s.find132pattern([-1, 3, 2, 0])
