0e692e1c4a12af74ae0ed86313a5666cf48f2275
Dmitry Odnorog <vibiy@yandex.ru>
2022-05-10 15:20:35 +0300

    595. Big Countries

---
+++ 595_big_countries.sql
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/big-countries/
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000;
