239c269e90921faa56b55e33b6bd947feaae6022
Dmitry Odnorog <vibiy@yandex.ru>
2022-05-13 00:07:09 +0300

    1484. Group Sold Products By The Date

---
+++ 1484_group_sold_products_by_the_date.sql
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/group-sold-products-by-the-date/
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product ASC) AS products
+FROM
+    activities
+GROUP BY
+    sell_date
+ORDER BY
+    sell_date;
