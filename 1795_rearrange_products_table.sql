55b358e02d8d081a81cf94b11b1a1cedac5f2a9b
Dmitry Odnorog <vibiy@yandex.ru>
2022-05-13 22:52:06 +0300

    1795. Rearrange Products Table

---
+++ 1795_rearrange_products_table.sql
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/rearrange-products-table/
+
+SELECT
+    *
+FROM (
+    SELECT
+        product_id,
+        'store1' AS store,
+        store1 AS price
+    FROM
+        Products
+
+    UNION ALL
+
+    SELECT
+        product_id,
+        'store2' AS store,
+        store2 AS price
+    FROM
+        Products
+
+    UNION ALL
+
+    SELECT
+        product_id,
+        'store3' AS store,
+        store3 AS price
+    FROM
+        Products
+
+    ORDER BY
+        product_id
+) AS temp
+WHERE
+    price IS NOT NULL;
