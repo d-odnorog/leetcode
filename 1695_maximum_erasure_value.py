1705486cd4320281fbb36abad0d118e73d6d243e
Dmitry Odnorog <vibiy@yandex.ru>
2022-06-13 14:38:32 +0300

    1695. Maximum Erasure Value

---
+++ 1695_maximum_erasure_value.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/maximum-erasure-value/
+from typing import List
+
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        seen = set()  # track visited elements in the window
+        res = i = tot = 0
+        for j in range(len(nums)):
+            x = nums[j]
+            # adjust the left bound of sliding window until you get all unique elements
+            while i < j and x in seen:
+                seen.remove(nums[i])
+                tot -= nums[i]
+                i += 1
+            tot += x
+            seen.add(x)
+            res = max(res, tot)
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.maximumUniqueSubarray([4, 2, 4, 5, 6]) == 17
+    assert s.maximumUniqueSubarray([5, 2, 1, 2, 5, 2, 1, 2, 5]) == 8
