40a38f619b7a972fb57895c1a8b93ad20b01ce9e
Dmitry Odnorog <vibiy@yandex.ru>
2022-06-30 22:29:59 +0300

    1423. Maximum Points You Can Obtain from Cards

---
+++ 1423_maximum_points_you_can_obtain_from_cards.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+from typing import List
+
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        total = sum(cardPoints)
+
+        remaining_length = n - k
+        subarray_sum = sum(cardPoints[:remaining_length])
+
+        min_sum = subarray_sum
+        for i in range(remaining_length, n):
+            # Update the sliding window sum to the subarray ending at index i
+            subarray_sum += cardPoints[i]
+            subarray_sum -= cardPoints[i - remaining_length]
+            # Update min_sum to track the overall minimum sum so far
+            min_sum = min(min_sum, subarray_sum)
+        return total - min_sum
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.maxScore([1, 2, 3, 4, 5, 6, 1], 3) == 12
+    assert s.maxScore([2, 2, 2], 2) == 4
+    assert s.maxScore([9, 7, 7, 9, 7, 7, 9], 7) == 55
