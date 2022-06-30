f492e2884981239b0afe854b2910bb154465e329
Dmitry Odnorog <vibiy@yandex.ru>
2022-06-30 22:32:06 +0300

    665. Non-decreasing Array

---
+++ 665_non-decreasing_array.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/non-decreasing-array/
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        is_non_decreasing = False
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                if is_non_decreasing:
+                    return False
+                is_non_decreasing = True
+                if i >= 2 and nums[i - 2] > nums[i]:
+                    nums[i] = nums[i - 1]
+        return True
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.checkPossibility([4, 2, 3])
+    assert not s.checkPossibility([4, 2, 1])
