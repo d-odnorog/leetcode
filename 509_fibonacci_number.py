c521a99aac677a1b370a809df4f3ac115d395c85
Dmitry Odnorog <vibiy@yandex.ru>
2022-07-24 12:39:59 +0300

    509. Fibonacci Number. Black

--- 509_fibonacci_number.py
+++ 509_fibonacci_number.py
@@ -6,7 +6,7 @@ class Solution:
         if n in [0, 1]:
             return n
         else:
-            return self.fib(n-1) + self.fib(n-2)
+            return self.fib(n - 1) + self.fib(n - 2)
 
 
 class Solution2:
