ac0923fd11deea160c2f942e13b1ec3d6aad474a
Dmitry Odnorog <vibiy@yandex.ru>
2022-08-05 22:24:36 +0300

    377. Combination Sum IV

---
+++ 377_combination_sum_IV.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/combination-sum-iv/
+from typing import List
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+
+        dp[0] = 1
+
+        for i in range(1, target + 1):
+            for num in nums:
+                num_before = i - num
+                if num_before >= 0:
+                    dp[i] += dp[num_before]
+
+        return dp[target]
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.combinationSum4([1, 2, 3], 4)
+    assert s.combinationSum4([9], 3) == 0
