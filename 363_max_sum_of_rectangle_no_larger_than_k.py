87e5c18d59691dc386c4ac04cedc708d9d3cd5e5
Dmitry Odnorog <vibiy@yandex.ru>
2022-08-28 00:47:57 +0300

    363. Max Sum of Rectangle No Larger Than K

---
+++ 363_max_sum_of_rectangle_no_larger_than_k.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+from bisect import bisect_left, insort
+from typing import List
+
+
+class Solution:
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        res = -float('inf')
+
+        for l in range(n):
+            rowSums = [0] * m
+            for r in range(l, n):
+                colSums = [0]
+                colSum = 0
+                for i in range(m):
+                    rowSums[i] += matrix[i][r]
+                    colSum += rowSums[i]
+                    diff = colSum - k
+                    idx = bisect_left(colSums, diff)
+                    if idx < len(colSums):
+                        if colSums[idx] == diff:
+                            return k
+                        else:
+                            res = max(res, colSum - colSums[idx])
+                    insort(colSums, colSum)
+
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert s.maxSumSubmatrix([[1, 0, 1], [0, -2, 3]], 2) == 2
+    assert s.maxSumSubmatrix([[2, 2, -1]], 3) == 3
