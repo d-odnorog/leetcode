c8bdea02ebfc39094430f9631caed65f7f43a6f6
Dmitry Odnorog <vibiy@yandex.ru>
2022-08-30 23:38:01 +0300

    200. Number of Islands

---
+++ 200_number_of_islands.py
@@ -0,0 +1,62 @@
+# https://leetcode.com/problems/number-of-islands/
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, i, j):
+            if (
+                i < 0
+                or j < 0
+                or i >= len(grid)
+                or j >= len(grid[0])
+                or grid[i][j] != '1'
+            ):
+                return
+
+            grid[i][j] = '#'
+
+            dfs(grid, i + 1, j)
+            dfs(grid, i - 1, j)
+            dfs(grid, i, j + 1)
+            dfs(grid, i, j - 1)
+
+        count = 0
+
+        if not grid:
+            return 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(grid, i, j)
+                    count += 1
+
+        return count
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    assert (
+        s.numIslands(
+            [
+                ['1', '1', '1', '1', '0'],
+                ['1', '1', '0', '1', '0'],
+                ['1', '1', '0', '0', '0'],
+                ['0', '0', '0', '0', '0'],
+            ]
+        )
+        == 1
+    )
+    assert (
+        s.numIslands(
+            [
+                ['1', '1', '0', '0', '0'],
+                ['1', '1', '0', '0', '0'],
+                ['0', '0', '1', '0', '0'],
+                ['0', '0', '0', '1', '1'],
+            ]
+        )
+        == 3
+    )
