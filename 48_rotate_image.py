c613a2c2727c8c9e0bc973483cdb4d3f8caede8f
Dmitry Odnorog <vibiy@yandex.ru>
2022-08-30 23:32:45 +0300

    48. Rotate Image

---
+++ 48_rotate_image.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/rotate-image/
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+
+
+class Solution2:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+
+
+if __name__ == '__main__':
+    for s in (Solution(), Solution2()):
+        matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        s.rotate(matrix)
+        assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+        matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+        s.rotate(matrix)
+        assert matrix == [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
